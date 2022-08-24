00657ba1ec005839be13a9d66616cc5f3aa21068
Kimberly <kimolanga123@gmail.com>
2022-08-24 14:23:11 +0300

    create departments html and its root view

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-
+  path('', views.home, name='home'),
+  path('about/',views.about,name='about'),
+  path('departments/',views.departments,name='departments'),
 ]
 
 
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def departments(request):
+    return render(request, 'departments.html')
